e8c0a52de30b7172156ebbeb15564b7dbddf81fb
Christophe-Saury <95085659+Christophe-Saury@users.noreply.github.com>
2022-06-17 14:22:12 +0200

    Add files via upload

---
+++ projet_app_info/page_gestion_profil_VF/vue/headeredit.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head><title> Gestion des profils </title>
+    <link rel="stylesheet" href="../vue/css/GestionTicketsv2.css"/>
+	<link href="../vue/headers-footer/navbar.css" rel="stylesheet">
+	<link href="../vue/headers-footer/footer.css" rel="stylesheet">
+
+
+    </head>
+
+
+
+
+    
+    <body>
